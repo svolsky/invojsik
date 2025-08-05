be58b0c55cd7896a8ecb09ab3a10864d1afbc68b
stas.v <stas.volsky@gmail.com>
2025-08-05 22:43:37 +0200

    fix front warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import InvoiceForm from './InvoiceForm';
 import Settings from './Settings';
 import './App.css';
